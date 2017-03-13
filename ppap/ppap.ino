15938989107dd144167153d1f82636a4d15afaa3
lofa40444 <as0921015717@gmail.com>
2017-03-13 20:36:21 +0800

    Add files via upload

---
+++ ppap/ppap.ino
@@ -0,0 +1,27 @@
+float sensorValue;
+  float sensorVoltage;
+  float R1=0;
+  float R2=1114000;
+  int Vin=5;
+  int i;
+ 
+
+
+void setup() 
+{
+ Serial.begin(9600);
+
+}
+
+void loop() 
+{
+  for (i=0;i<=5;i++)
+    sensorValue = 0.7* sensorValue + 0.3* analogRead(A1);
+  sensorVoltage = sensorValue * 5 / 1024;
+  // sensorVoltage= Vin * R1 / (R1+R2)
+  R1 = sensorVoltage * (R2) / Vin ;
+  Serial.print(R1); Serial.print(",  ");
+  Serial.print(sensorVoltage); Serial.print(",  ");
+  Serial.println(sensorValue);
+  delay(500);
+}
---
+++ 實驗二.ino
@@ -0,0 +1,23 @@
+
+   float Analogin ;
+
+  float Vin;
+void setup() {
+   Serial.begin(9600);}
+
+void loop() {
+  int i; 
+  for (i=0;i<=5;i++)
+  {
+    Analogin = 0.7* Analogin + 0.3* analogRead(A0);
+  Vin = Analogin * 5 / 1024;
+  Serial.println(Vin);
+ delay(500);
+  
+  }
+
+}
+
+
+
+
